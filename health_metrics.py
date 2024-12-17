9b25f3a9dab7238449681db704242dc020312e90
LongJeffreySilver <87135689+LongJeffreySilver@users.noreply.github.com>
2024-12-17 13:49:06 +0100

    One json event with host metrics in HTTP endpoint
    The metrics extracted by the script are parsed to json and then treated as a single event. The HTTP endpoint now shown both metrics dynamically.

--- health_metrics.py
+++ health_metrics.py
@@ -6,8 +6,7 @@ def main():
     psutil.cpu_percent(interval=None)  # sin usar el valor
     cpu_usage = psutil.cpu_percent(interval=1)  # segunda llamada, valor real
     mem_info = psutil.virtual_memory().available / (1024*1024)  # en MB
-    print(f"cpu_usage {cpu_usage}")
-    print(f"memory_available_mb {mem_info}")
+    print(f'{{"cpu_usage":{cpu_usage},"memory_mb":{mem_info}}}')
 
 if __name__ == "__main__":
     main()
